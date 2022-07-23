b6892c6c8f4bd24656ca9dde89fad9697fef3710
YReis <yago.safini@hotmail.com>
2022-07-23 17:42:34 -0300

    axios configs

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,7 +2,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import Axios from "axios";
+
+
 
 //Styling imports
 
@@ -21,7 +22,11 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Visibility from "@mui/icons-material/Visibility";
 import IconButton from "@mui/material/IconButton";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
-
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import Axios from "axios";
+//urls
+import variables from "../variables";
 
 
 export default function Login(props){
@@ -31,8 +36,20 @@ export default function Login(props){
         username:"",
         password:""
     })
+
     const [loading,setLoading] = useState(false)
+    const [showIcon,setShowicon] = useState(true)
     
+    function onSubmit(e){
+       Axios({
+        data:login,
+        method:"POST",
+        withcredentials:true,
+        url:`${variables.back}/login`
+       }).then((res)=>{
+        res.data.erro?alert(res.data.mensagem) :navigate("/home")
+       });
+    }
     
     return(
         <>
@@ -55,7 +72,7 @@ export default function Login(props){
                 required="true"
                 value={login.username}
                 onChange={(e)=>{
-                    setLogin([...login],e.target.value)
+                    setLogin({...login,["username"]:e.target.value})
                 }}
                 endAdornment={<InputAdornment position="end">
                     {<AccountCircleIcon/>}
@@ -63,11 +80,31 @@ export default function Login(props){
                 >
                 </OutlinedInput>
             </FormControl>
-
+            <FormControl>
+                <InputLabel htmlFor="password">password</InputLabel>
+                <OutlinedInput name="password"
+                id="password"
+                type={showIcon?"text" : "password"}
+                text="faca seu login"
+                required="true"
+                value={login.password}
+                onChange={(e)=>{
+                    setLogin({...login,['password']:e.target.value})
+                }}
+                endAdornment={<InputAdornment position="end"><IconButton onClick={(e)=>{
+                setShowicon(!showIcon)
+                }}>{!showIcon ? <VisibilityIcon /> : <VisibilityOffIcon/>  }
+                    </IconButton>
+                </InputAdornment>}
+                >
+                </OutlinedInput>
+            </FormControl>
+              <Button onClick={()=>{
+                onSubmit()
+              }}>entrar</Button>
             </Stack>
         </form>
         </>
     )
-
 
 }
---
+++ src/variables.js
@@ -0,0 +1,4 @@
+const variables={
+    back:"http://192.168.0.140:4000",
+}
+export default variables
